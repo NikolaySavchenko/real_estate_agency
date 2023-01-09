79732101050c61aff7d1e7182767d56e5ad676e4
Nikolay Savchenko <7548568@gmail.com>
2023-01-09 21:49:18 +0300

    Setup Flat changelist page

--- property/models.py
+++ property/models.py
@@ -47,7 +47,7 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
     new_building = models.BooleanField('True — новостройка, False — старое здание',
-                                       blank=True, db_index=True)
+                                       blank=True, null=True, db_index=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
