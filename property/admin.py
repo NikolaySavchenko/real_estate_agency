8e2befbf40d04501af6a333c1e157a682a7eab3e
Nikolay Savchenko <7548568@gmail.com>
2023-01-13 20:50:22 +0300

    Parse owners phone numbers

--- property/admin.py
+++ property/admin.py
@@ -5,7 +5,7 @@ from .models import *
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('owner', 'town', 'address',)
     readonly_fields = ('created_at',)
-    list_display = ('address', 'price', 'new_building',
+    list_display = ('address', 'owner_pure_phone', 'price', 'new_building',
                     'construction_year', 'town')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony',)
