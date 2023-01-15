6e3e9b8c68c804d9efd87d6e44b0b76fa8790f95
Nikolay Savchenko <7548568@gmail.com>
2023-01-15 19:48:14 +0300

    Add owner info in Flat_admin

--- property/admin.py
+++ property/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 
 from .models import *
 
+class OwnerInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ('owner',)
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address',)
     readonly_fields = ('created_at',)
     list_display = ('id', 'address', 'price', 'new_building',
                     'construction_year', 'town')
+    inlines = [OwnerInline]
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony',)
     raw_id_fields = ('liked_by',)
@@ -19,6 +25,7 @@ class OwnerAdmin(admin.ModelAdmin):
     search_fields = ('full_name',)
     list_display = ('full_name', )
     raw_id_fields = ('flats',)
+
 
 
 admin.site.register(Flat, FlatAdmin)
